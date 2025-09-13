8cfd2bedbee187648dab55214de79e7c3d2af019
Kuro-30677 <Nuttakorn2549@gmail.com>
2025-09-13 17:12:26 +0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import './App.css';
 function App() {
   return (
     <div className="App" style={{ maxWidth: 400, margin: '40px auto', padding: 24, border: '1px solid #eee', borderRadius: 8 }}>
-      <h2>Todo List (Zustand)</h2>
+      <h2>Todo List </h2>
       <AddTodo />
       <TodoList />
     </div>
